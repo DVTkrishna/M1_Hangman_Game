d5d4b36055b5e12219f54f525e752c8867caba2b
DVTkrishna <daddanalakrishna09@gmail.comgit config --global user.name  DVTkrishnagit config --global user.email daddanalakrishna09@gmail.comgit config --global user.email daddanalakrishna09@gmail.com>
2022-03-03 01:11:49 +0530

    commit

--- 3_Implementation/project main/src/HANGMAN_CODE.c
+++ 3_Implementation/project main/src/HANGMAN_CODE.c
@@ -1,188 +1,147 @@
-#include "HANGMAN_CODE.H"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <time.h>
+#include <string.h>
 
+#define WORDS 10
+#define WORDLEN 40
+#define CHANCE 6
 
-void Print() {
+bool srand_called = false;
 
-   /*******
-   Shows the instructions to the user
-   Instructions on how to play the Hangman Game
-   *********/
+int i_rnd(int i) {
+    if (!srand_called) {
+        srand(time(NULL) << 10);
+        srand_called = true;
+    }
+    return rand() % i;
+}
 
-   printf("\t\t\t\t\tWelcome to the Hangman Game.\n"); //load();
-   getch();
-   clear;
-   printf("Game instructions:\n");
-   printf("\n-> The player must discover the word by guessing letters one at a time.\n\n->"
-      " On each wrong guess, a body part is added to a picture of a hanged man. \n\n-> "
-      "Player is allowed 5 mistakes, corresponding to hangman's head, body, left arm, right arm, left leg. At the 6th mistake the right leg is drawn, and the game is over. \n\n->"
-      " If a letter is repeated more than once, the repeating occurrences are always considered as an error, even if the first time was correct.\n\n->"
-      " Once the hanged man is complete, the player loses the game.\n");
-   printf("\n\nPress any key to play game...");
-   getch();
-} //end if void Print
+char* decrypt(char* code) {
+	int hash = ((strlen(code) - 3) / 3) + 2;
+	char* decrypt = malloc(hash);
+	char* toFree = decrypt;
+	char* word = code;
+	for (int ch = *code; ch != '\0'; ch = *(++code))
+	{
+		if((code - word + 2) % 3  == 1){
+			*(decrypt++) = ch - (word - code + 1) - hash;
+		}
+	}
+	*decrypt = '\0';
+	return toFree;
+}
 
-void load() {
+void printBody(int mistakes, char* body) {
+	printf("\tMistakes :%d\n", mistakes);
+	switch(mistakes) {
 
-   /******
-   shows the loading animation
-   *****/
-   int r, q;
-   printf("\n\n\n\n\n\n\n\n\n\t\t\t\t\t    loading...\n\n\n\n\t\t\t\t\t");
-   for (r = 1; r <= 20; r++) {
-      for (q = 0; q <= 100000000; q++); //to display the character slowly
-      printf("%c", 219);
-   }
-   printf("\n\n\t\t\t\t(please press any key to continue)");
-   getch();
-} //end of void load
+		case 6: body[6] = '\\'; break;
+		case 5: body[5] = '/'; break;
+		case 4: body[4] = '\\'; break;
+		case 3: body[3] = '|'; break;
+		case 2: body[2] = '/'; break;
+		case 1: body[1] = ')', body[0] = '('; break;
+		default: break;
 
-int introduction(char * answer) {
+	}
 
-   /*****************
-   This is the introduction of the game.
-   It asks the user to enter a word to be guessed
-   OR he/she will let the program choose for the
-   words to be guessed.
-   *****************/
+	printf("\t ___\n"
+	       "\t|         |\n"
+	       "\t|        %c %c\n"
+	       "\t|        %c%c%c\n"
+	       "\t|        %c %c\n"
+	       "\t|             \n"
+	       "\t|             ", body[0], body[1], body[2],
+	       body[3], body[4], body[5], body[6]);
+}
 
-   char answer1[100]; {
-      printf("\n\n\n\n\n\n\n\n\t\t\t\t\t\t\t\t\n");
-      printf("#    #       *      # *    #    ########            #        #                 *         #*      #					    \n");
-      printf("#    #      * *     #  *   #    #                  #  #    #   #              * *        #  *    #						\n");
-      printf("#    #     *   *    #   *  #    #                 #   #   #      #           *   *       #   *   #			            \n");
-      printf("######    * * * *   #    * #       #####         #      #         #         * * * *      #	*  #	            \n");
-      printf("#    #    *     *   #     *#    #      #        #       #         #         *     *      #     * #				        \n");
-      printf("#    #    *     *   #     *#    #      #       #        #         #         *     *      #      *#				        \n");
-      printf("#    #    *     *   #      #    ########      #          #         #        *     *      #       #  		            \n");
-      printf("\n\n     Welcome to the Hangman Game. \n-->This program was made by Asher <--\n        URL: asher\n");
-   }
+void printWord(char* guess, int len) {
+	printf("\t");
+	for (int i = 0; i < len; ++i)
+	{
+		printf("%c ", guess[i]);
+	}
+	printf("\n\n");
+}
 
-   //Ask the user for decision and save it to choice.
-   int MainMenu = 777;
-   while (MainMenu == 777) {
-      int decide;
-      printf("\nPress 1. Enter the Hangman Word (2 or more players).\nPress 2. Generate a random Hangman Word (You vs Computer).\nDecision: ");
-      scanf("%d", & decide);
-      getchar();
-      if (decide == 1) goto choice1;
-      else if (decide == 2) goto choice2;
-      else printf("\nIncorrect Input: ");
-      MainMenu = 777;
-   }
+int main() {
 
-   choice1: {
+	printf("\n\t Be aware you can be hanged!!.");
 
-      printf("\n\nEnter the word to be guessed: ");
-      scanf("%s", answer);
-      printf("\nYour input: %s", answer);
-      printf("\n\n(press any key to continue)");getch();clear;
-      return *answer;
-   }
+	printf("\n\n\t Rules : ");
+	printf("\n\t - Maximum 6 mistakes are allowed.");
+	printf("\n\t - All alphabet are in lower case.");
+	printf("\n\t - All words are name of very popular Websites. eg. Google");
+	printf("\n\t - If you enjoy continue, otherwise close it.");
 
-   choice2: {
+	printf("\n\t Syntax : Alphabet");
+	printf("\n\t Example : a \n\n");
 
-      randomizer;
-      char words[e][8] = {
-         "cat",
-         "dog",
-         "cow",
-         "rainbow",
-         "water",
-         "apple",
-         "love",
-         "cotton",
-         "camel",
-         "cloud"
-      };
-      int f;
-      f = rand() % 10;
-      //  printf("This is the random word: %s", words[f]);
-      printf("\nGenerated Successfully");
-      strcpy(answer, words[f]);
-      printf("\n\n(press any key to continue)");getch();clear;
-      return *answer;
-   }
+	char values[WORDS][WORDLEN] = {"N~mqOlJ^tZletXodeYgs","gCnDIfFQe^CdP^^B{hZpeLA^hv","7urtrtwQv{dt`>^}FaR]i]XUug^GI",
+									"aSwfXsxOsWAlXScVQmjAWJG","cruD=idduvUdr=gmcauCmg]","BQt`zncypFVjvIaTl]u=_?Aa}F",
+									"iLvkKdT`yu~mWj[^gcO|","jSiLyzJ=vPmnv^`N]^>ViAC^z_","xo|RqqhO|nNstjmzfiuoiFfhwtdh~",
+									"OHkttvxdp|[nnW]Drgaomdq"};
+	char *body = malloc(CHANCE+1);
 
-} //end for int introduction (char *answer) function
+	int id = i_rnd(WORDS);
+	char *word = decrypt(values[id]);
+	int len = strlen(word);
+	char *guessed = malloc(len);
+	char falseWord[CHANCE];
 
-void showHangman(int choice) {
+	memset(body, ' ', CHANCE+1);
+	memset(guessed, '_', len);
+	char guess;
+	bool found;
+	char* win;
 
-   /***************
-   This function show the hangman progress
-   of being hanged after each wrong try
-   ****************/
+	int mistakes = 0;
+	setvbuf(stdin, NULL, _IONBF, 0);
 
-   switch (choice) {
+	do {
 
-   case 0:
-      system("cls");
-      printf("\n\t||===== ");
-      printf("\n\t||    | ");
-      printf("\n\t||   %cO/", '\\');
-      printf("\n\t||    |                                              YOU LOSE THE GAME!");
-      printf("\n\t||   / %c", '\\');
-      printf("\n\t||      ");
-      break;
-   case 1:
-      system("cls");
-      printf("\n\t||===== ");
-      printf("\n\t||    | ");
-      printf("\n\t||   %cO/", '\\');
-      printf("\n\t||    | ");
-      printf("\n\t||     %c", '\\');
-      printf("\n\t||      ");
-      break;
-   case 2:
-      system("cls");
-      printf("\n\t||===== ");
-      printf("\n\t||    | ");
-      printf("\n\t||   %cO/", '\\');
-      printf("\n\t||    | ");
-      printf("\n\t||      ");
-      printf("\n\t||      ");
-      break;
-   case 3:
-      system("cls");
-      printf("\n\t||===== ");
-      printf("\n\t||    | ");
-      printf("\n\t||   %cO/", '\\');
-      printf("\n\t||      ");
-      printf("\n\t||      ");
-      printf("\n\t||      ");
-      break;
-   case 4:
-      system("cls");
-      printf("\n\t||===== ");
-      printf("\n\t||    | ");
-      printf("\n\t||   %cO ", '\\');
-      printf("\n\t||      ");
-      printf("\n\t||      ");
-      printf("\n\t||      ");
-      break;
-   case 5:
-      system("cls");
-      printf("\n\t||===== ");
-      printf("\n\t||    | ");
-      printf("\n\t||    O ");
-      printf("\n\t||      ");
-      printf("\n\t||      ");
-      printf("\n\t||      ");
-      break;
-   } //end of switch-case
-   return;
-} //end of showhangman
+		found = false;
+		printf("\n\n");
+		printBody(mistakes, body);
+		printf("\n\n");
+		printf("\tFalse Letters : ");
+		if(mistakes == 0) printf("None\n");
+		for (int i = 0; i < mistakes; ++i)
+		{
+			printf("%c", falseWord[i]);
+		}
+		printf("\n\n");
+		printWord(guessed, len);
+		printf("\tGive me a alphabet in lower case : ");
+		do {scanf("%c",&guess);} while ( getchar() != '\n' );
+		for (int i = 0; i < len; ++i)
+		{
+			if(word[i] == guess) {
+				found = true;
+				guessed[i] = guess;
+			}	
+		}
+		if(!found) {
+			falseWord[mistakes] = guess;
+			mistakes += 1;
+		}
+		win = strchr(guessed, '_');
+	}while(mistakes < CHANCE && win != NULL);
 
-void planeFig(void) {
+	if(win == NULL) {
+		printf("\n");
+		printWord(guessed, len);
+		printf("\n\tCongrats! You have won : %s\n\n", word);
+	} else {
+		printf("\n");
+		printBody(mistakes, body);
+		printf("\n\n\tBetter try next time. Word was %s\n\n", word);
+	}
 
-   /*show the HANGMAN*/
-
-   printf("\n\n\n");
-   printf("\tH A N G M A N");
-   printf("\n\t+------+");
-   printf("\n\t|      | ");
-   printf("\n\t|        ");
-   printf("\n\t|        ");
-   printf("\n\t|        ");
-   printf("\n\t|       ");
-   printf("\n\t========\n\n\n");
-} 
+	free(body);
+	free(word);
+	free(guessed);
+	return EXIT_SUCCESS;
+}
